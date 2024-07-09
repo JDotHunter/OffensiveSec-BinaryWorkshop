576475c03e054d0b0bf1728950503dc69eb12153
JDotHunter <JaylanNHunter3306@gmail.com>
2024-07-09 14:34:49 -0400

    Created a random number generator that syncs up with a random sequence of numbers.

--- sol.py
+++ sol.py
@@ -1,9 +1,21 @@
+
 from pwn import *
+from ctypes import CDLL
+
+libc = CDLL("libc.so.6")
+
+the_time = libc.srand(libc.time(0))
+
+time = libc.rand()
+
+print(time)
+
 
 io = process('./time')
 print(io.recvuntil(b'Enter your number: '))
-io.sendline(b'90')
+io.sendline(b'%d' % time)
 display = io.recvline()
 print(display)
+print(io.recvall())
 
 
