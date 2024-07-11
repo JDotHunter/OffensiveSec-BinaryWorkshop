3809ab4bc61c0aaf5b9314a9c2edb2ec7643e43d
JDotHunter <JaylanNHunter3306@gmail.com>
2024-07-11 14:15:32 -0400

    It's just a source file for babypwn

---
+++ BabyPwn/babypwn.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+struct __attribute__((__packed__)) data {
+  char buff[32];
+  int check;
+};
+
+void ignore(void)
+{
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stdin, NULL, _IONBF, 0);
+}
+
+void get_flag(void)
+{
+  char flag[1024] = { 0 };
+  FILE *fp = fopen("flag.txt", "r");
+  fgets(flag, 1023, fp);
+  printf(flag);
+}
+
+int main(void) 
+{
+  struct data name;
+  ignore(); /* ignore this function */
+
+  printf("What's your name?\n");
+//THIS IS THE VULNERABILITY!!! 
+//We have a 32-bit char array while attempting to read a 64-bit name.
+  fgets(name.buff, 64, stdin);
+  sleep(2);
+  printf("%s nice to meet you!\n", name.buff);
+  sleep(2);
+  printf("Binary exploitation is the best!\n");
+  sleep(2);
+  printf("Memory unsafe languages rely on coders to not make mistakes.\n");
+  sleep(2);
+  printf("But I don't worry, I write perfect code :)\n");
+  sleep(2);
+
+  if (name.check == 0x41414141) {
+    get_flag();
+  }
+
+  return 0;
+}
